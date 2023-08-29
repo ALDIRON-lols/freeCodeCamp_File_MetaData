4934ce081c23c44b33a9b5e7015abca717507154
ALDIRON-lols <gamerboy191001@gmail.com>
2023-08-29 17:45:24 +0500

    completed

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var cors = require('cors');
-require('dotenv').config()
+require('dotenv').config();
+const multer = require('multer');
+const upload = multer({dest: 'C:/Users/Huzaifah/Pictures/'})
 
 var app = express();
 
@@ -11,7 +13,11 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.post('/api/fileanalyse', upload.single("upfile"), (req, res)=>{
+  console.log("Body Object: ",req.body);
+  console.log("File Object: ",req.file);
+  res.json({name: req.file.originalname, type: req.file.mimetype, size: req.file.size});
+});
 
 
 const port = process.env.PORT || 3000;
